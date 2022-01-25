6717c0c6e2d9ad4e0b93e79393f8fd0f69e7389a
Jesús García <jesusfgarciavaldez@gmail.com>
2022-01-24 18:18:51 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const ejercicio1 = [
+  3, 100, 85, 64, 46, 39, 40, 30, 20, 24, 25, 6, 10, 54, 82, 71, 67, 77, 17, 29,
+  19, 88, 456, 13, 23, 24,
+];
+
+const ejercicio2 = [
+  {
+    nombre: "Gabriel",
+    edad: 22,
+    esFamiliar: false,
+  },
+  {
+    nombre: "Jaime",
+    edad: 15,
+    esFamiliar: true,
+  },
+  {
+    nombre: "María",
+    edad: 18,
+    esFamiliar: true,
+  },
+  {
+    nombre: "Angel",
+    edad: 19,
+    esFamiliar: true,
+  },
+  {
+    nombre: "Fer",
+    edad: 18,
+    esFamiliar: true,
+  },
+  {
+    nombre: "Rachel",
+    edad: 30,
+    esFamiliar: true,
+  },
+];
