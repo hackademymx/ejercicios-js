002cf8e148e307a582278bfd5bc83ccb5f19b562
AlejandraBM <alejandrabenitezmtz@gmail.com>
2022-01-29 23:54:57 -0600

    ejercicios resueltos

--- script.js
+++ script.js
@@ -35,3 +35,32 @@ const ejercicio2 = [
     esFamiliar: true,
   },
 ];
+
+// Ejercicio 1
+let res = []
+ejercicio1.forEach(num => {
+  if (num % 2 != 0 & num & 3 != 0 & num % 5 != 0){
+    res.push(num)
+  }
+})
+console.log(res)
+
+// Ejercicio 2
+let familia = []
+ejercicio2.forEach(invitado => {
+  if (invitado.esFamiliar){
+    if (invitado.edad >= 18){
+      familia.push(invitado.nombre)
+    }
+  }
+})
+console.log(familia)
+
+
+// Ejercicio 3
+let fib = [0,1]
+for(let i = 2;i <50; i++){
+  let j = fib[i - 1] + fib[i - 2]
+  fib.push(j)
+}
+console.log(fib)
